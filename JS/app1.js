9149be989e5f4e18377fa6861e274e3739e472ec
Jess Malesh <jamalesh84@gmail.com>
2016-05-02 15:35:44 -0700

    functions for round and game winner

--- JS/app1.js
+++ JS/app1.js
@@ -1,10 +1,15 @@
 function draw() {
-  var logo = document.getElementById('logo');
-  var ctx = logo.getContext('2d');
+  var canvas = document.getElementById('logo');
+  var ctx = canvas.getContext('2d');
   ctx.beginPath();
   ctx.moveTo(75,75);
   ctx.bezierCurveTo(37.5, 275, 262.5, 275, 225, 75);
   ctx.lineTo(75, 75);
+  ctx.moveTo(150, 75);
+  ctx.lineTo(150, 140);
+  ctx.lineTo(85, 185);
+  ctx.moveTo(150,140);
+  ctx.lineTo(215, 185);
   ctx.stroke();
 }
 
