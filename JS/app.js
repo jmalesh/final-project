affb4cc3d302968f97692e4891b1cb163e6c7911
Gin <vienlyt@gmail.com>
2016-05-02 14:15:28 -0700

    updating app

--- JS/app.js
+++ JS/app.js
@@ -1,34 +1,99 @@
 var itemArray = new Array(3);
 var classicItemInfo = [['rock', '../assets/rock.jpeg'],
                 ['paper', '../assets/paper.jpeg'],
-                ['scissor', '../assets/scissors_PNG25.png']];
+                ['scissor', '../assets/scissor.png']];
 
 var taItemInfo = [];
-var customItemInfo;
+var customItemInfo = [];
 
-// function RPSItem(name, url) {
-//   this.name = name;
-//   this.url = url;
-//   this.beat = null;
-// }
+var userWins = 0;
+var rounds = 7;
+var initialNumberOfRounds = 7;
+
+function RPSItem(name, url) {
+  this.name = name;
+  this.url = url;
+  this.beat = null;
+}
 
 RPSItem.prototype.setBeat = function(other) {
   this.beat = other;
 };
 
-function makeGameArray(inputInfo){
-  for(var i = 0; i < inputInfo.length; i++){
+function makeGameArray(inputInfo) {
+  for(var i = 0; i < inputInfo.length; i++) {
     var item = new RPSItem(inputInfo[i][0], inputInfo[i][1]);
     itemArray[i] = item;
   }
+
   itemArray[0].setBeat(itemArray[itemArray.length - 1]);
-  itemArray[1].setBeat(itemArray[0]);
-  itemArray[2].setBeat(itemArray[1]);
+  for (var j = 1; j < itemArray.length; j++) {
+    itemArray[j].setBeat(itemArray[j - 1]);
+  }
 }
+// populate array with classic items
 makeGameArray(classicItemInfo);
 
 //generate random number for computer choice
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-getRandomInt(arrayItem.length);
+// getRandomInt(arrayItem.length);
+
+// 0 = draw
+// -1 = user lost
+// 1 = user win
+function oneRound(userInput) {
+  var computerInput = itemArray[getRandomInt(itemArray.length)];
+  console.log('comp input is ' + computerInput.name);
+  if (userInput.beat === computerInput.beat) {
+    return 0;
+  } else if (userInput.beat === computerInput) {
+    userWins++;
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+// console.log(oneRound(itemArray[0]));
+
+var interactionPanel = document.getElementById('interaction-panel');
+
+var userInputContainer = document.getElementById('user-input-container');
+userInputContainer.addEventListener('click', processUserSelection);
+
+function processUserSelection(event) {
+  if (rounds) {
+    var clickedOnDiv = parseInt(event.target.parentNode.id);
+    // console.log('user clicked on div number ' + clickedOnDiv);
+    if (!isNaN(clickedOnDiv)) {
+      var userInput = itemArray[parseInt(clickedOnDiv)];
+      console.log('user input is ' + userInput.name);
+      console.log(oneRound(userInput));
+      // reportOneRound();
+    }
+    rounds--;
+    if (rounds == 0) {
+      displayResult();
+    }
+  }
+}
+
+function displayResult() {
+  console.log('done');
+  console.log('You\'ve won ' + userWins + ' out of ' + initialNumberOfRounds);
+}
+
+// function reportOneRound() {
+//   var oneRoundReport = document.createElement('div');
+//   var computerChoice = document.createElement('img');
+//   computerChoice.src = 'assets/paper.jpeg';
+//   var text = document.createTextNode('computer chose ');
+//   oneRoundReport.appendChild(text);
+//   oneRoundReport.appendChild(computerChoice);
+//   while (interactionPanel.firstChild) {
+//     interactionPanel.removeChild(interactionPanel.firstChild);
+//   }
+//   interactionPanel.appendChild(oneRoundReport);
+// }
